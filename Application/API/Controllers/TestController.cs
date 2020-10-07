c7bc9f22bf3ad9f48cbe83518bb677975842baf7
Azizkhon Ishankhonov <Azizxon_uz@hotmail.com>
2020-10-07 10:17:04 +0300

    feat: add application layer

---
+++ Application/API/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Summaries;
+        }
+        private static readonly string[] Summaries = {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestController> _logger;
+    }
+}
